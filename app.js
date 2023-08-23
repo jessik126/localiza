109a48bee93adf9c3b2262008aa9b30442f58146
jessik126 <jessik126@gmail.com>
2023-08-23 16:27:09 -0300

    conexão bd mongo

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('ola mundo');
+const express = require('express');
+const database = require('./configs/database');
+
+// conexão com o banco de dados
+database.mongoose.connect(database.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=> {
+    console.log('conexão estabelecida com banco de dados.')
+}).catch(err => {
+    console.log('nao conseguiu conectar com banco de dados', err);
+    process.exit();
+});
+
+// criando uma aplicação express
+const app = express();
+
+// definindo que vamos enviar e receber json na requisição
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const PORT = 3000;
+const HOST = '0.0.0.0'
+
+app.listen(PORT, HOST, () =>{
+    console.log(`servidor executando na porta ${PORT}`);
+});
