86a1f39f409974eb232bc8e2b6aca9cccc1e4047
jessik126 <jessik126@gmail.com>
2023-09-01 20:10:17 -0300

    Utilização de swagger

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const database = require('./configs/database');
 
 // conexão com o banco de dados
@@ -42,6 +45,40 @@ app.use('/', reservaRouter);
 
 const PORT = 3000;
 const HOST = '0.0.0.0'
+
+const options = {
+    definition: {
+    openapi: "3.1.0",
+    info: {
+        title: "LogRocket Express API with Swagger",
+        version: "0.1.0",
+        description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+        license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+        },
+    },
+    servers: [
+        {
+        url: "http://localhost:3000",
+        },
+    ],
+    },
+    apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
 
 app.listen(PORT, HOST, () =>{
     console.log(`servidor executando na porta ${PORT}`);
--- routes/carroRouter.js
+++ routes/carroRouter.js
@@ -1,3 +1,68 @@
+/**
+* @swagger
+* components:
+*     schemas:
+*         Carro:
+*             type: object
+*             required:
+*                 - placa
+*                 - ano
+*                 - modelo                  
+*                 - tipo                  
+*                 - quilometragem                  
+*                 - diaria                  
+*             properties:
+*                 id:
+*                     type: string
+*                     description: The auto-generated id of the book
+*                 placa:
+*                     type: string
+*                 ano:
+*                     type: number
+*                 modelo:
+*                     type: string
+*                 tipo:
+*                     type: string
+*                 quilometragem:
+*                     type: number
+*                 diaria:
+*                     type: number
+*                 observacao:
+*                     type: string           
+*             example:
+*                 placa: ABC1234
+*                 ano: 2000
+*                 modelo: uno
+*                 tipo: utilitario
+*                 quilometragem: 10000
+*                 diaria: 100
+*
+* tags:
+*   name: Carro
+*   description: API Carro
+* /carros:
+*   post:
+*     summary: Criar novo carro
+*     tags: [Carros]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/Carro'
+*     responses:
+*       200:
+*         description: Carro criado.
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Carro'
+*       500:
+*         description: Erro servidor
+*
+*/
+
+
 const express = require('express');
 const router = express.Router();
 
